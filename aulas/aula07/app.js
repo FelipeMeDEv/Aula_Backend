9a199ffd0fae1ab273bd1aaf0f65b9bd51b04ff5
Felipe Melo <felipemsoares22@gmail.com>
2025-09-18 10:50:13 -0300

    codigos da aula 07

---
+++ aulas/aula07/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+app.use('/', indexRouter);
+
+const tarefas = [];
+
+app.get('/tarefas', (req, res) => {
+    res.json(tarefas);
+})
+
+app.post('/tarefas', (req, res) =>{
+    const novaTarefa = {
+        id: tarefas.length + 1,
+        nome: req.body.nome,
+        concluida: false
+    };
+    tarefas.push(novaTarefa);
+    res.status(201).json(novaTarefa);
+    
+    res.status(201).json({id: 1, nome: "Estudar para P1", concluida: false});
+});
+
+module.exports = app;
