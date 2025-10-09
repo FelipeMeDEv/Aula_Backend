7d827d2a92acba4e1b3e8d93c8d8b6ef7c5dae8b
Felipe Melo <felipemsoares22@gmail.com>
2025-10-09 10:44:42 -0300

    adicionando aula 8

---
+++ aulas/aula08/database.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require('mongodb');
+
+const url = ""
+
+const client = new MongoClient(url);
+
+async function conectar (){
+    try{
+        await client.connect();
+        console.log("Conectado")
+        return client.db("agenda");
+    } catch(e) {
+        console.log("Erro ao conectar no MongoDB", e.message)
+    }
+}
+
+
+module.exports = conectar;
