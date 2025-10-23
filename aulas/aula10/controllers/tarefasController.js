ccb4cd32874cf6e94f020c29eedc0d81b897e7fc
Felipe Melo <felipemsoares22@gmail.com>
2025-10-23 10:42:46 -0300

    adicionando aula 10 final

--- aulas/aula10/controllers/tarefasController.js
+++ aulas/aula10/controllers/tarefasController.js
@@ -1,17 +1,24 @@
+const mongoose = require("mongoose");
 const Tarefa = require("../models/tarefasModel");
 
 async function criar(req, res) {
-  const tarefaEncontrada = await Tarefa.create({
-    nome: req.body.nome,
-    concluida: false,
-  });
-  return res.status(201).json({
-    id: tarefaEncontrada._id,
-    nome: tarefaEncontrada.nome,
-    concluida: tarefaEncontrada.concluida,
-  });
+  try {
+    const tarefaEncontrada = await Tarefa.create({
+      nome: req.body.nome,
+      concluida: false,
+    });
+    return res.status(201).json({
+      id: tarefaEncontrada._id,
+      nome: tarefaEncontrada.nome,
+      concluida: tarefaEncontrada.concluida,
+    });
+  } catch (err) {
+    if (err.errors) {
+      return res.status(422).json({ msg: err.errors["nome"].message });
+    }
+    return res.status(500).json({msg: "Deu ruim"});
+  }
 }
-
 async function listar(req, res) {
   const tarefas = await Tarefa.find({});
 
@@ -20,6 +27,11 @@ async function listar(req, res) {
 
 async function buscar(req, res, next) {
   const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ msg: "ID invalido" });
+  }
+
   const tarefaEncontrada = await Tarefa.findOne({ _id: id });
   if (tarefaEncontrada) {
     req.tarefa = {
@@ -29,26 +41,34 @@ async function buscar(req, res, next) {
     };
     return next();
   }
-  return res.status(404).json({msg: "Tarefa não encontrada"});
+  return res.status(404).json({ msg: "Tarefa não encontrada" });
 }
 
 function exibir(req, res) {
-  return res.json({ id: 1 });
+  return res.json(req.tarefa);
 }
 
 async function atualizar(req, res) {
-  const { id } = req.params;
-  const tarefaEncontrada = await Tarefa.findOneAndUpdate(
-    { _id: id },
-    { ...req.body }
-  );
-  return res.json({
-    id: tarefaEncontrada._id,
-    nome: tarefaEncontrada.nome,
-    concluida: tarefaEncontrada.concluida,
-  });
+  try {
+    const { id } = req.params;
+    const tarefaEncontrada = await Tarefa.findOneAndUpdate(
+      { _id: id },
+      { ...req.body },
+      { new: true, runValidators:true }
+    );
+    return res.json({
+      id: tarefaEncontrada._id,
+      nome: tarefaEncontrada.nome,
+      concluida: tarefaEncontrada.concluida,
+    });
+  } catch (err) {
+    if (err.errors) {
+      return res.status(422).json({
+        msg: err.errors["nome"].message,
+      });
+    }
+  }
 }
-
 async function remover(req, res) {
   const { id } = req.params;
   const tarefaRemovida = await Tarefa.findOneAndDelete({ _id: id });
--- aulas/aula10/test/tarefasRouter.test.js
+++ aulas/aula10/test/tarefasRouter.test.js
@@ -17,6 +17,16 @@ describe('Teste do recurso /tarefas', () => {
         expect(response.body.concluida).toBe(false);
         id = response.body.id;
     });
+    test('POST / deve retorna 422', async() => {
+        const response = await request.post(url).send({nome : "a"});
+        expect(response.status).toBe(422);
+        expect(response.body.msg).toBe("Nome da Tarefa deve ter pelo menos 3 caracteres")
+    });
+    test('POST / deve retorna 422', async() => {
+        const response = await request.post(url).send({nome : "   "});
+        expect(response.status).toBe(422);
+        expect(response.body.msg).toBe("Nome da tarefa é obrigatório")
+    });
 
     test('GET / deve retorna 200', async() =>{
         const response = await request.get(url);
@@ -28,14 +38,23 @@ describe('Teste do recurso /tarefas', () => {
         const response = await request.get(`${url}/${id}`);
         expect(response.status).toBe(200)
         expect(response.body.id).toBeDefined();
+        expect(response.body.nome).toBe("Estudar")
+        expect(response.body.concluida).toBe(false);
     });
 
+    test('GET /id deve retorna 400', async() => {
+        const response = await request.get(`${url}/0`);
+        expect(response.status).toBe(400)
+        expect(response.body.msg).toBe("ID invalido");
+    });
+
+    
+
     test('GET /id deve retorna 404', async() => {
-        const response = await request.get(`${url}/000000000000000000000000`);
+        const response = await request.put(`${url}/000000000000000000000000`);
         expect(response.status).toBe(404)
         expect(response.body.msg).toBe("Tarefa não encontrada");
     });
-
 
 
     test('PUT /id deve retorna 404', async() => {
@@ -48,15 +67,45 @@ describe('Teste do recurso /tarefas', () => {
         const response = await request.put(`${url}/${id}`).send({ nome: "Estudar REST", concluida:true });
         expect(response.status).toBe(200)
         expect(response.body.id).toBeDefined();
+        expect(response.body.nome).toBe("Estudar REST")
+        expect(response.body.concluida).toBe(true);
     });
+
+    test('PUT /id deve retorna 422', async() => {
+        const response = await request.put(`${url}/${id}`).send({nome : "   "});
+        expect(response.status).toBe(422)
+        expect(response.body.msg).toBe("Nome da tarefa é obrigatório")
+    });
+    test('PUT /id deve retorna 422', async() => {
+        const response = await request.put(`${url}/${id}`).send({nome : "a"});
+        expect(response.status).toBe(422)
+        expect(response.body.msg).toBe("Nome da Tarefa deve ter pelo menos 3 caracteres")
+    });
+
+
+    
+    test('PUT /id deve retorna 400', async() => {
+        const response = await request.put(`${url}/0`);
+        expect(response.status).toBe(400)
+        expect(response.body.msg).toBe("ID invalido");
+    });
+
 
     test('DELETE /id deve retorna 204', async() => {
         const response = await request.delete(`${url}/${id}`);
         expect(response.status).toBe(204)
     });
 
+    
+    test('DELETE /id deve retorna 400', async() => {
+        const response = await request.delete(`${url}/0`);
+        expect(response.status).toBe(400)
+        expect(response.body.msg).toBe("ID invalido");
+    });
+
+
     test('DELETE /id deve retorna 404', async() => {
-        const response = await request.delete(`${url}/000000000000000000000000`);
+        const response = await request.delete(`${url}/${id}`);
         expect(response.status).toBe(404)
         expect(response.body.msg).toBe("Tarefa não encontrada");
     });
